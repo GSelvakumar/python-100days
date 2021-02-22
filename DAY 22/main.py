97d83fbde05f3abad2e9b2dfe582fd5c6b01e642
GSelvakumar <gselvaofficial@outlook.com>
2021-02-22 22:07:20 +0530

    py

--- DAY 22/main.py
+++ DAY 22/main.py
@@ -1,5 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +12,8 @@ screen.tracer(0)  # turns off the animation, so need to turn on the animation ma
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -18,6 +23,28 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    #Detect the collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        #need to bounce
+        ball.bounce_y()
+
+    #Detect the collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        #here 50 is the size of the paddle and the 320 is the axis line of the paddle.
+        ball.bounce_x()
+
+    #Detect Right paddle misses the ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    #Detect Left paddle misses the ball
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
