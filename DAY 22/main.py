720408b4c121cc66f0adfb91f6bfd2548d25c24f
GSelvakumar <gselvaofficial@outlook.com>
2021-02-22 08:01:52 +0530

    py

--- DAY 22/main.py
+++ DAY 22/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,24 +10,14 @@ screen.tracer(0)  # turns off the animation, so need to turn on the animation ma
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 
-
-
-
-# def go_up():
-#     new_y = paddle.ycor() + 20
-#     paddle.goto(paddle.xcor(), new_y)
-
-
-# def go_down():
-#     new_y = paddle.ycor() - 20
-#     paddle.goto(paddle.xcor(), new_y)
-
-
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
     screen.update()
+
 screen.exitonclick()
--- DAY 22/paddle.py
+++ DAY 22/paddle.py
@@ -3,9 +3,20 @@ from turtle import Turtle
 class Paddle(Turtle):   #gets the properties of Turtle class
 
     def __init__(self, position):
-        super.__init__()   #used to utilize the properties of Turtle class
+        super().__init__()   #used to utilize the properties of Turtle class
         self.shape("square")
         self.color("white")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
         self.goto(position)
+
+        
+    def go_up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+
+    def go_down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
+
