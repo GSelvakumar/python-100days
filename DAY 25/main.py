a9035bf79c14f13f9f48e0048be563ec4e704bd9
GSelvakumar <gselvaofficial@outlook.com>
2021-02-25 19:04:40 +0530

    py

---
+++ DAY 25/main.py
@@ -0,0 +1,25 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+# import csv
+
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperatures.append(row[1])
+#     print(temperatures)
+
+import pandas 
+
+data = pandas.read_csv("weather_data.csv")
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(len(temp_list))
+
+average = sum(temp_list) / len(temp_list)
+print(average)
