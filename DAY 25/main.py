cb6147599055a0d878e8e9ab6d45cacec1d585a2
GSelvakumar <gselvaofficial@outlook.com>
2021-02-26 16:41:22 +0530

    DAY 25

--- DAY 25/main.py
+++ DAY 25/main.py
@@ -15,11 +15,37 @@
 import pandas 
 
 data = pandas.read_csv("weather_data.csv")
-data_dict = data.to_dict()
-print(data_dict)
+# data_dict = data.to_dict()
+# print(data_dict)
 
-temp_list = data["temp"].to_list()
-print(len(temp_list))
+# temp_list = data["temp"].to_list()
+# print(len(temp_list))
 
-average = sum(temp_list) / len(temp_list)
-print(average)
+# print(data["temp"].mean())
+# print(data["temp"].max())
+
+# #Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+# #Get data in row
+# print(data[data.day == "Monday"])
+
+# #To get the maximum temperature from the temp column
+# print(data[data.temp == data.temp.max()])
+
+#To get monday temperature in Farh.
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+# monday_temp_f = monday_temp * 9/5 + 32
+# print(monday_temp_f)
+
+#create a dataframe from scratch
+
+data_dict = {
+    "students": ["Amy", "Anwar"],
+    "scores": [45, 54]
+}
+
+data = pandas.Dataframe(data_dict)
+data.to_csv("new_data.csv")
