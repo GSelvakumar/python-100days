cf91eef95e1d89dc89e5e572112c736a3ba5b18e
GSelvakumar <gselvaofficial@outlook.com>
2021-03-13 15:46:44 +0530

    central park data census model

--- DAY 25/main.py
+++ DAY 25/main.py
@@ -1,51 +1,19 @@
-# with open("weather_data.csv") as data_file:
-#     data = data_file.readlines()
-#     print(data)
+import pandas as pd
 
-# import csv
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     temperatures = []
-#     for row in data:
-#         if row[1] != "temp":
-#             temperatures.append(row[1])
-#     print(temperatures)
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
 
-import pandas 
-
-data = pandas.read_csv("weather_data.csv")
-# data_dict = data.to_dict()
-# print(data_dict)
-
-# temp_list = data["temp"].to_list()
-# print(len(temp_list))
-
-# print(data["temp"].mean())
-# print(data["temp"].max())
-
-# #Get Data in Columns
-# print(data["condition"])
-# print(data.condition)
-
-# #Get data in row
-# print(data[data.day == "Monday"])
-
-# #To get the maximum temperature from the temp column
-# print(data[data.temp == data.temp.max()])
-
-#To get monday temperature in Farh.
-# monday = data[data.day == "Monday"]
-# monday_temp = int(monday.temp)
-# monday_temp_f = monday_temp * 9/5 + 32
-# print(monday_temp_f)
-
-#create a dataframe from scratch
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
 
 data_dict = {
-    "students": ["Amy", "Anwar"],
-    "scores": [45, 54]
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
 }
 
-data = pandas.Dataframe(data_dict)
-data.to_csv("new_data.csv")
+df = pd.DataFrame(data_dict)
+df.to_csv("squrriel_count.csv")
