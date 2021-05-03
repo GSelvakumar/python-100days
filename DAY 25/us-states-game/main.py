d2e923f84a435a8664c251e87408b029b1e1abb4
GSelvakumar <gselvaofficial@outlook.com>
2021-05-03 08:26:00 +0530

    states_game done

---
+++ DAY 25/us-states-game/main.py
@@ -0,0 +1,46 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image) #used to add a img as shape
+turtle.shape(image)
+
+data = pd.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+    print(answer_state)
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pd.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+screen.exitonclick()
+
+
+
+#code to get the x and y corr from the image:)
+
+# def get_mouse_click_corr(x, y):
+#     print(x, y)
+
+# turtle.onscreenclick(get_mouse_click_corr)
+# turtle.mainloop() #used to keep the turtle screen open so must remove exit on click
