510126b94343cc7457571d95d6879e5ffdb13a8d
GSelvakumar <gselvaofficial@outlook.com>
2021-05-20 08:04:57 +0530

    basic list comphrension files

---
+++ DAY 26/squaring_numbers.py
@@ -0,0 +1,5 @@
+numbers = [1, 2, 23, 4, 55, 6]
+
+new_list = [num * num for num in numbers]
+
+print(new_list)
---
+++ DAY 26/take-even-out.py
@@ -0,0 +1,5 @@
+numbers = [1, 21, 23, 4, 44, 32, 67]
+
+result = [num for num in numbers if num % 2 == 0]
+
+print(result)
